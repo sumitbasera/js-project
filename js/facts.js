c137df6496c8e8a716cd3769bcf11154e44209a3
sumit <sunilbasera1705@gmail.com>
2021-10-22 12:22:38 +0530

    Add files via upload

---
+++ js/facts.js
@@ -0,0 +1,17 @@
+let requestURLs = 'http://numbersapi.com/random/year?json';
+let requests = new XMLHttpRequest();
+requests.open('GET', requestURLs);
+requests.responseType = 'json';
+requests.send();
+
+requests.onload = function() {
+    const facts = requests.response;
+    let dinak = facts.number;
+    let solution = facts.text;
+    document.getElementById("din").innerHTML = dinak;
+     document.getElementById("fac").innerHTML = solution;
+  }
+
+  function refresh(){
+    window.location.reload("Refresh")
+  }
